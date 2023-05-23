6c6ae0b0d2ee734bbe8171f14d15b45b19d2e24b
Mady93 <popamariana1.pm@gmai.com>
2023-05-23 17:39:59 +0200

    aument progress bar all infinito da padre a figlio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component,OnInit,OnDestroy } from '@angular/core';
-import { AppModule } from './app.module';
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'cap06';
   //il costruttoreinizzializza proprieta e metodi
 
@@ -26,6 +26,18 @@ export class AppComponent implements OnInit{
         this.avanzamento = 100;
       }
     }, 1200);
+
+    // AGGIORNAMENTO AUTOMATICO ALL'INFINITO
+    /*
+     setInterval(() => {
+      if (this.avanzamento + this.aumento <= 100) {
+        this.avanzamento += this.aumento;
+      } else {
+        this.avanzamento = 0;
+      }
+    }, 1200);
+  }
+    */
   }
   
 }
