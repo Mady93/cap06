2f83e2bcf28876dcab028e769282935e6b945fd4
Mady93 <popamariana1.pm@gmai.com>
2023-05-23 15:56:01 +0200

    second commit

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -7,8 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProgressBarComponent implements OnInit {
 
-  @Input() progress:number;
-
   constructor() { }
 
   ngOnInit() {
