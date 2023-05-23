128a4e8e78b6aa4d1caee3589fcba34409dbc36d
Mady93 <popamariana1.pm@gmai.com>
2023-05-23 17:22:34 +0200

    aumento progress bar da padre a figlio commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,OnDestroy } from '@angular/core';
+import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cap06';
+  //il costruttoreinizzializza proprieta e metodi
 
-  avanzamento:number = 20;
+  avanzamento: number = 0;
+  aumento: number = 20;
+
+  constructor() {
+
+   
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      if (this.avanzamento + this.aumento < 100) {
+        this.avanzamento += this.aumento;
+      } else {
+        this.avanzamento = 100;
+      }
+    }, 1200);
+  }
+  
 }
